2807408ac48b5712be98c336aa889b67fca92c9b
Nikhil <nikhildadab@hotmail.com>
2023-11-24 18:48:00 +0530

    Cleared Imports

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -1,6 +1,4 @@
 import 'dart:typed_data';
-
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:instagram_clone/resources/storage_methods.dart';
